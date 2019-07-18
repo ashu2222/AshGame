de135903e384f2d0c8b9e3348a62d120c93993af
Aashish Dolia <735023@student.tor.loc>
2019-07-18 17:04:03 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const{app, BrowserWindow}=require('electron')
+const path=require('path')
+const url=require('url')
+
+let win;
+
+
+function createwindow(){
+win=new BrowserWindow({width:800,height:800})
+
+
+}
+win.loadurl(url.format({
+pathname:path.join(__dirname, 'main.html'),
+protocol:'file:',
+slashes: true
+
+
+}))
